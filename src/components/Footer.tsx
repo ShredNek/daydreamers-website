43032bd1a58f292ecf7d7adaa9f211b2a78c8b19
Daniel Lee <danielleemusic98@gmail.com>
2025-10-27 21:47:01 +1100

    Finish footer stylings

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,35 @@
-import RoundedButtonLink from "../components/RoundedButtonLink";
-import { FaFacebookF, FaInstagram, FaSpotify, FaMusic } from "react-icons/fa6";
+import { FaFacebookF, FaInstagram, FaMusic, FaSpotify } from "react-icons/fa6";
 import { PiHeartFill } from "react-icons/pi";
+import RoundedButtonLink from "../components/RoundedButtonLink";
 
 export default function Footer() {
 	return (
 		<footer>
-			<div className="social-links">
-				<RoundedButtonLink imageChild={<FaFacebookF />} />
-				<RoundedButtonLink imageChild={<FaInstagram />} />
-				<RoundedButtonLink imageChild={<FaSpotify />} />
-				<RoundedButtonLink imageChild={<FaMusic />} />
-			</div>
-			<ul className="extra-links">
-				<li className="link">See our Press Kit</li>
-			</ul>
-			<p className="extra-links">
-				<strong>© 2015-{new Date().getFullYear()} Day Dreamers</strong>
-			</p>
-			<div className="developer-message">
-				<p>Home-made with</p>
-				<PiHeartFill />
-				<p>by</p>
-				<a target="_blank" href="https://github.com/shredNek/" rel="noreferrer">
-					Daniel Lee.
-				</a>
+			<div className="footer-content">
+				<div className="social-links">
+					<RoundedButtonLink imageChild={<FaFacebookF />} />
+					<RoundedButtonLink imageChild={<FaInstagram />} />
+					<RoundedButtonLink imageChild={<FaSpotify />} />
+					<RoundedButtonLink imageChild={<FaMusic />} />
+				</div>
+				<ul className="extra-links">
+					<li className="link">Press Kit</li>
+				</ul>
+				<p className="extra-links">
+					<strong>© 2015-{new Date().getFullYear()} Day Dreamers</strong>
+				</p>
+				<div className="developer-message">
+					<p>Home-made with</p>
+					<PiHeartFill />
+					<p>by</p>
+					<a
+						target="_blank"
+						href="https://github.com/shredNek/"
+						rel="noreferrer"
+					>
+						Daniel Lee.
+					</a>
+				</div>
 			</div>
 		</footer>
 	);
