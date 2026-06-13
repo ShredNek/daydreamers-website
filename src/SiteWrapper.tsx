579b4b2b5037fc87fdc944daa213cec8ef949b72
Daniel Lee <danielleemusic98@gmail.com>
2026-06-13 10:53:17 +1000

    change embed to vimeo

--- src/SiteWrapper.tsx
+++ src/SiteWrapper.tsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer.tsx";
 import LazyImage from "./components/LazyImage.tsx";
 import NavHeader from "./components/NavHeader.tsx";
 import "./styles/views/_site-wrapper.scss";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { useGlobalStartupProcedures } from "./hooks/useGlobalFunctions.tsx";
 import { AppContext } from "./utils/AppContext.tsx";
@@ -14,6 +14,7 @@ type SiteWrapperComponent = {
 };
 
 export default function SiteWrapper({ hideBackground }: SiteWrapperComponent) {
+	const [videoLoaded, setVideoLoaded] = useState(false);
 	const { dialogContent } = useContext(AppContext);
 	const globalStartupProcedures = useGlobalStartupProcedures();
 
@@ -24,22 +25,30 @@ export default function SiteWrapper({ hideBackground }: SiteWrapperComponent) {
 			<div className="website-content">
 				<NavHeader hideBackground={hideBackground} />
 				<div className="background-visuals">
-					<div className="site-backdrop" id="site-backdrop" />
-					<div className="photo-backdrop" id="photo-backdrop">
+					<div className="video-backdrop">
+						<iframe
+							allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+							onLoad={() => {
+								setVideoLoaded(true);
+							}}
+							referrerPolicy="strict-origin-when-cross-origin"
+							src="https://player.vimeo.com/video/1200922975?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=1&amp;muted=1&amp;loop=1"
+							title="DayDreamers_y2k-OMV"
+						/>
+					</div>
+					<script defer src="https://player.vimeo.com/api/player.js" />
+					<div
+						className={`photo-backdrop${videoLoaded ? " fade-out" : " "}`}
+						id="photo-backdrop"
+					>
 						<LazyImage
 							alt="Hero shot of your local wacky band, Day Dreamers"
 							highQualitySrc={highQualHero}
 							lowQualitySrc={lowQualHero}
 						/>
 					</div>
-					<iframe
-						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-						className="video-backdrop"
-						referrerPolicy="strict-origin-when-cross-origin"
-						src="https://www.youtube.com/embed/BXBw2Dvl2fQ?si=y2lXcPFlcs_w2K3G&amp;controls=0&amp;start=3&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;showinfo=0"
-						title="YouTube video player"
-					/>
 				</div>
+				<div className="site-backdrop" id="site-backdrop" />
 				<div className="content-container">
 					<Outlet />
 				</div>
